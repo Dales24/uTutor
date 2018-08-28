597255ba58bd093d677a807b5eeba84f0dd2b137
Patrick Daley <34901029+Dales24@users.noreply.github.com>
2018-08-27 21:27:02 -0400

    test

--- app/src/main/java/com/dan/ututor/Student/Student.java
+++ app/src/main/java/com/dan/ututor/Student/Student.java
@@ -5,7 +5,7 @@ public class Student {
     private String email;
     private String password;
     private int ID;
-
+    public int name;
     public Student() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
