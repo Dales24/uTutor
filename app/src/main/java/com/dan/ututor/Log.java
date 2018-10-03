4fd97edad2cf27167088535217e8c2e1f2d74ad5
Patrick Daley <34901029+Dales24@users.noreply.github.com>
2018-10-02 20:42:05 -0400

    login feature. Need a case that searchs db

--- app/src/main/java/com/dan/ututor/Log.java
+++ app/src/main/java/com/dan/ututor/Log.java
@@ -3,6 +3,7 @@ package com.dan.ututor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import com.dan.ututor.Queries.LoginQueries;
@@ -12,30 +13,23 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Log extends AppCompatActivity {
-
+    Button login;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    public void loginClicked(View view){
+    login = (Button) findViewById(R.id.login);
 
-        //Retrieve login type from radio buttons
+    firebaseDatabase = FirebaseDatabase.getInstance();
 
-        String personType="";
-        CompoundButton studentRadio =  findViewById(R.id.radioStudent);
-        CompoundButton tutorRadio =  findViewById(R.id.radioTutor);
+    databaseReference = firebaseDatabase.getReference("https://capstone-71d9c.firebaseio.com/");
 
-        if(studentRadio.isChecked())
-        {
-            LoginQueries.LoginStudent(view);
-        }
-        else if(tutorRadio.isChecked())
-        {
-            LoginQueries.LoginTutor(view);
-        }
-
+        login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
 
     }
