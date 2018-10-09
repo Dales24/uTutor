4dd9a4cba7e240a56ed66507f7021c2b8b9d8fc8
Patrick Daley <34901029+Dales24@users.noreply.github.com>
2018-10-08 21:00:29 -0400

    Starting to work on the home feed. We need to pull from database. I added a list view that allows this but now we need to pull information

--- app/src/main/java/com/dan/ututor/System/MainActivity.java
+++ app/src/main/java/com/dan/ututor/System/MainActivity.java
@@ -3,9 +3,11 @@ package com.dan.ututor.System;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBarDrawerToggle;
+
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout di;
+
     public class StudentHome {
 
         protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/dan/ututor/System/TutorHome.java
+++ app/src/main/java/com/dan/ututor/System/TutorHome.java
@@ -1,12 +1,45 @@
 package com.dan.ututor.System;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import com.dan.ututor.R;
 
 public class TutorHome {
+
+// pass profiles from database to array
+     String[] profiles = new String[1000];
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_home);
+
+        ListView listView = (ListView)findViewByID(R.id.listView);
+
+    }
+
+    class CustomAdapter extends BaseAdapter{
+@Override
+public int getCount(){
+    return profiles.length;
+}
+@Override
+        public Object getItem(int i){
+    return null;
+}
+        @Override
+        public long getItemId(int i){
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }}
     // display interface
 
