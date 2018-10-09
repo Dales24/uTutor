65b9732968e45919ca303e5533dae979799af7c9
Patrick Daley <34901029+Dales24@users.noreply.github.com>
2018-10-08 21:23:01 -0400

    Reading from database. needs conditons

--- app/src/main/java/com/dan/ututor/System/TutorHome.java
+++ app/src/main/java/com/dan/ututor/System/TutorHome.java
@@ -1,22 +1,45 @@
 package com.dan.ututor.System;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import com.dan.ututor.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class TutorHome {
 
 // pass profiles from database to array
-     String[] profiles = new String[1000];
+     String[] profiles;
 
-
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_home);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("https://capstone-71d9c.firebaseio.com/Tutor");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value  = dataSnapshot.getValue(String.class);
+                       
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        })
+
 
         ListView listView = (ListView)findViewByID(R.id.listView);
 
