9fcf17874ca441a7b165e8cae93e3f25687c0524
Patrick Daley <34901029+Dales24@users.noreply.github.com>
2018-12-08 13:30:54 -0500

    feed logic cont... xml needs to be mod

--- app/src/main/java/com/dan/ututor/System/TutorHome.java
+++ app/src/main/java/com/dan/ututor/System/TutorHome.java
@@ -1,9 +1,20 @@
 package com.dan.ututor.System;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
 import com.dan.ututor.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,14 +24,24 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class TutorHome extends AppCompatActivity {
     FirebaseAuth mAuth;
     DatabaseReference databaseReference;
     DatabaseReference databaseReference2;
     FirebaseDatabase firebaseDatabase;
+    private TextView name;
+    private TextView location;
+    private TextView major;
+    private TextView description;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_home);
+        name = findViewById(R.id.name);
+        location= findViewById(R.id.loc);
+       major = findViewById(R.id.maj);
+        description = findViewById(R.id.des);
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference().child("Relation");
         databaseReference.keepSynced(true);
@@ -35,6 +56,7 @@ public class TutorHome extends AppCompatActivity {
                         String keys = datas.getKey();
                         databaseReference2 = FirebaseDatabase.getInstance().getReference().child("Students").child(keys);
                 String sName     =  databaseReference2.child("Name").toString();
+
                         System.out.println("testing " + sName);
                         sendName(databaseReference2);
                     }
@@ -53,7 +75,13 @@ public class TutorHome extends AppCompatActivity {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         String sName = dataSnapshot.child("Name").getValue(String.class);
-
+                        String location1 = dataSnapshot.child("Location").getValue(String.class);
+                        String descrip = dataSnapshot.child("Description").getValue(String.class);
+                        String major1 = dataSnapshot.child("Major").getValue(String.class);
+                        name.setText(sName);
+                        location.setText(location1);
+                        major.setText(major1);
+                        description.setText(descrip);
                         System.out.println("testing " + sName);
                     }
 
